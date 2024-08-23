06f2087acd187d3c872aa9ce34969dac55673280
Marcos Costa <marcoscosta@Marcoss-MacBook-Pro.local>
2024-08-23 18:47:20 -0400

    first commit: config environment

---
+++ iosChallenge/iosChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosChallenge
+//
+//  Created by Marcos Costa on 23/08/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+
+
+}
+
